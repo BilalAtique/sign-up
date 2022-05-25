9b6e3321b2190bd80ab513085267af5d411e9b6a
Bilal Atique <74999164+MuhammadBilalBinAtique@users.noreply.github.com>
2022-05-25 16:54:22 +0500

    Add styles in form

--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import "./SignUp.scss"
+import "./SignUp.scss";
 
 export default function SignUp() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isPending, setIsPending] = useState(false);
     const handleSubmit = (e) => {
         e.preventDefault();
+        setIsPending(true);
         const userData = { username, email, password };
         fetch("http://localhost:3030/api/user", {
             method: "POST",
@@ -15,36 +17,45 @@ export default function SignUp() {
             },
             body: JSON.stringify(userData),
         })
-            .then(() => console.log("User is added"))
-            .catch((err) => console.error(err));
+            .then(() => {
+                console.log("User is added");
+                setIsPending(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setIsPending(false);
+            });
     };
     return (
         <div className="form-container">
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    className="username"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="email"
-                    className="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    className="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Register</button>
-            </form>
+            <div className="container">
+                <h1>Sign Up</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="username"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        className="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        className="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {!isPending && <button type="submit">Register</button>}
+                    {isPending && <button disabled type="submit">...</button>}
+                </form>
+            </div>
         </div>
     );
 }
